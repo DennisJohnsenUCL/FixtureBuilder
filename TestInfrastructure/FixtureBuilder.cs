1f28abc35e37f1e66a741620e016c7002d2cabc3
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-10-10 17:27:31 +0200

    Removed summary todos from all classes. Remove test todos from classes with tests.

--- TestInfrastructure/FixtureBuilder.cs
+++ TestInfrastructure/FixtureBuilder.cs
@@ -4,55 +4,54 @@ using System.Runtime.CompilerServices;
 
 namespace TestUtilities
 {
-    //TODO: Summaries
-    public class FixtureBuilder<TEntity> where TEntity : class
-    {
-        private readonly TEntity _fixture;
-        public TEntity Build() => _fixture;
+	public class FixtureBuilder<TEntity> where TEntity : class
+	{
+		private readonly TEntity _fixture;
+		public TEntity Build() => _fixture;
 
-        public FixtureBuilder()
-        {
-            _fixture = (TEntity)RuntimeHelpers.GetUninitializedObject(typeof(TEntity));
-        }
+		public FixtureBuilder()
+		{
+			_fixture = (TEntity)RuntimeHelpers.GetUninitializedObject(typeof(TEntity));
+		}
 
-        public FixtureBuilder<TEntity> With<TProp>(Expression<Func<TEntity, TProp>> expr, TProp value)
-        {
-            var propInfo = GetPropertyInfo(expr);
+		public FixtureBuilder<TEntity> With<TProp>(Expression<Func<TEntity, TProp>> expr, TProp value)
+		{
+			var propInfo = GetPropertyInfo(expr);
 
-            var backingField = _fixture.GetType().GetField($"<{propInfo.Name}>k__BackingField",
-                BindingFlags.Instance | BindingFlags.NonPublic)
-                ?? throw new InvalidOperationException($"Backing field not found for property {propInfo.Name}");
+			var backingField = _fixture.GetType().GetField($"<{propInfo.Name}>k__BackingField",
+				BindingFlags.Instance | BindingFlags.NonPublic)
+				?? throw new InvalidOperationException($"Backing field not found for property {propInfo.Name}");
 
-            backingField.SetValue(_fixture, value);
+			backingField.SetValue(_fixture, value);
 
-            return this;
-        }
+			return this;
+		}
 
-        public FixtureBuilder<TEntity> WithFieldUnsafe(string fieldName, object value)
-        {
-            var fieldInfo = _fixture.GetType().GetField(fieldName,
-                BindingFlags.Instance | BindingFlags.NonPublic)
-                ?? throw new InvalidOperationException($"Field '{fieldName}' not found.");
+		public FixtureBuilder<TEntity> WithFieldUnsafe(string fieldName, object value)
+		{
+			var fieldInfo = _fixture.GetType().GetField(fieldName,
+				BindingFlags.Instance | BindingFlags.NonPublic)
+				?? throw new InvalidOperationException($"Field '{fieldName}' not found.");
 
-            fieldInfo.SetValue(_fixture, value);
+			fieldInfo.SetValue(_fixture, value);
 
-            return this;
-        }
+			return this;
+		}
 
-        private static PropertyInfo GetPropertyInfo<TProp>(Expression<Func<TEntity, TProp>> expr)
-        {
-            if (expr.Body is MemberExpression memberExpr && memberExpr.Member is PropertyInfo propInfo)
-            {
-                return propInfo;
-            }
+		private static PropertyInfo GetPropertyInfo<TProp>(Expression<Func<TEntity, TProp>> expr)
+		{
+			if (expr.Body is MemberExpression memberExpr && memberExpr.Member is PropertyInfo propInfo)
+			{
+				return propInfo;
+			}
 
-            if (expr.Body is UnaryExpression unary && unary.Operand is MemberExpression innerMember
-                && innerMember.Member is PropertyInfo innerProp)
-            {
-                return innerProp;
-            }
+			if (expr.Body is UnaryExpression unary && unary.Operand is MemberExpression innerMember
+				&& innerMember.Member is PropertyInfo innerProp)
+			{
+				return innerProp;
+			}
 
-            throw new ArgumentException("Expression must be a property access", nameof(expr));
-        }
-    }
+			throw new ArgumentException("Expression must be a property access", nameof(expr));
+		}
+	}
 }
