41642d554d3b2a5e81b649979b9f01b8cd284aae
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-10-31 19:56:41 +0100

    Moved NetArchTest custom rules and extensions to Shared.TestUtilities.
    Added a project reference to Shared.TestUtilities from Ordering.ArchitectureTests.

---
+++ Shared.TestUtilities/NetArchTest/CustomRules.cs
@@ -0,0 +1,28 @@
+﻿using Mono.Cecil;
+using NetArchTest.Rules;
+
+namespace Shared.TestUtilities.NetArchTest
+{
+	public class ImplementsInterfaceInHierarchy(Type _interfaceType) : ICustomRule
+	{
+		public bool MeetsRule(TypeDefinition type)
+		{
+			while (type != null)
+			{
+				foreach (var item in type.Interfaces.Select(x => x.InterfaceType))
+				{
+					if (item.Namespace == _interfaceType.Namespace &&
+						item.Name == _interfaceType.Name) return true;
+					if (item.GetElementType().FullName == _interfaceType.FullName) return true;
+				}
+				if (type.BaseType != null)
+				{
+					try { type = type.BaseType.Resolve(); }
+					catch { break; }
+				}
+				else type = null!;
+			}
+			return false;
+		}
+	}
+}
---
+++ Shared.TestUtilities/NetArchTest/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using NetArchTest.Rules;
+
+namespace Shared.TestUtilities.NetArchTest
+{
+	public static class Extensions
+	{
+		public static PredicateList ImplementInterfaceInHierarchy(this Predicate predicates, Type interfaceType)
+		{
+			return predicates.MeetCustomRule(new ImplementsInterfaceInHierarchy(interfaceType));
+		}
+
+		public static PredicateList ImplementInterfaceInHierarchy<T>(this Predicate predicates)
+		{
+			return predicates.MeetCustomRule(new ImplementsInterfaceInHierarchy(typeof(T)));
+		}
+	}
+}
