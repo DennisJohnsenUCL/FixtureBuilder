16b7e77de86ddfc178ce2759f5c608a31bab5d47
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-11-01 20:41:31 +0100

    Created an interface for rule templates and created rule templates for class naming, namespaces, and infrastructure leakage.

---
+++ Shared.TestUtilities/NetArchTest/IVariableRule.cs
@@ -0,0 +1,9 @@
+﻿using NetArchTest.Rules;
+
+namespace Shared.TestUtilities.NetArchTest
+{
+	public interface IVariableRule
+	{
+		TestResult GetResultFor(Types types);
+	}
+}
---
+++ Shared.TestUtilities/NetArchTest/VariableRules.cs
@@ -0,0 +1,60 @@
+﻿using NetArchTest.Rules;
+
+namespace Shared.TestUtilities.NetArchTest
+{
+	public class NamingRule(Type _interfaceType, string _expectedName) : IVariableRule
+	{
+		public TestResult GetResultFor(Types types)
+		{
+			return types
+				.That()
+				.AreNotAbstract()
+				.And()
+				.ImplementInterfaceInHierarchy(_interfaceType)
+				.Should()
+				.HaveNameEndingWith(_expectedName)
+				.GetResult();
+		}
+	}
+
+	public class NamespaceRule(Type _interfaceType, string _expectedNamespace) : IVariableRule
+	{
+		public TestResult GetResultFor(Types types)
+		{
+			return types
+				.That()
+				.AreNotAbstract()
+				.And()
+				.ImplementInterfaceInHierarchy(_interfaceType)
+				.Should()
+				.ResideInNamespace(_expectedNamespace)
+				.GetResult();
+		}
+	}
+
+	internal class EntityFrameworkLeakageRule(string _namespace) : IVariableRule
+	{
+		public TestResult GetResultFor(Types types)
+		{
+			return types
+				.That()
+				.ResideInNamespace(_namespace)
+				.ShouldNot()
+				.HaveDependencyOnAny("Microsoft.EntityFrameworkCore")
+				.GetResult();
+		}
+	}
+
+	internal class DependencyInjectionLeakageRule(string _namespace) : IVariableRule
+	{
+		public TestResult GetResultFor(Types types)
+		{
+			return types
+				.That()
+				.ResideInNamespace(_namespace)
+				.ShouldNot()
+				.HaveDependencyOnAny("Microsoft.Extensions.DependencyInjection")
+				.GetResult();
+		}
+	}
+}
