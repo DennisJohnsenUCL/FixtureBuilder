6d926ce25562d80fde194dbcef0207402dd679f8
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-12-15 18:01:49 +0100

    Refactored CastToCollection into helper methods.

--- FixtureBuilder/Helpers/CollectionHelpers.cs
+++ FixtureBuilder/Helpers/CollectionHelpers.cs
@@ -11,84 +11,32 @@ namespace FixtureBuilder.Helpers
         {
             if (fieldType.IsArray)
             {
-                var valuesList = values.Cast<object>().ToList();
-                var elementType = fieldType.GetElementType()!;
-                var array = Array.CreateInstance(elementType, valuesList.Count);
-                for (int i = 0; i < valuesList.Count; i++)
-                {
-                    array.SetValue(valuesList[i], i);
-                }
-                return array;
+                return CastToArray(fieldType, values);
             }
 
-            if (fieldType.IsInterface)
+            else if (fieldType.IsInterface)
             {
-                Type concreteType;
-                var genericTypeDef = fieldType.GetGenericTypeDefinition();
-                var elementType = fieldType.GetGenericArguments()[0];
-
-                if (genericTypeDef == typeof(IList<>) ||
-                    genericTypeDef == typeof(IReadOnlyList<>) ||
-                    genericTypeDef == typeof(IEnumerable<>) ||
-                    genericTypeDef == typeof(IReadOnlyCollection<>) ||
-                    genericTypeDef == typeof(ICollection<>))
-                {
-                    concreteType = typeof(List<>);
-                }
-                else if (genericTypeDef == typeof(ISet<>) ||
-                    genericTypeDef == typeof(IReadOnlySet<>))
-                {
-                    concreteType = typeof(HashSet<>);
-                }
-                else if (genericTypeDef == typeof(IImmutableList<>)) concreteType = typeof(ImmutableList<>);
-                else if (genericTypeDef == typeof(IImmutableSet<>)) concreteType = typeof(ImmutableHashSet<>);
-                else throw new InvalidOperationException($"Unsupported interface type: {fieldType.Name}");
-
-                if (concreteType != null) fieldType = concreteType.MakeGenericType(elementType);
+                fieldType = GetConcreteType(fieldType);
             }
 
             if (fieldType.IsGenericType)
             {
                 var genericTypeDef = fieldType.GetGenericTypeDefinition();
                 var elementType = fieldType.GetGenericArguments()[0];
+                var typedList = typeof(Enumerable)
+                    .GetMethod("Cast")!
+                    .MakeGenericMethod(elementType)
+                    .Invoke(null, [values]) as IEnumerable
+                    ?? throw new InvalidOperationException($"Failed to cast values to IEnumerable<{elementType.Name}>");
 
-                if (genericTypeDef.FullName?.StartsWith("System.Collections.Immutable.Immutable") == true)
+                if (genericTypeDef.FullName?.StartsWith("System.Collections.Immutable.Immutable") ?? false)
                 {
-                    var factoryTypeName = genericTypeDef.FullName.Replace("`1", "");
-                    var factoryType = Type.GetType(factoryTypeName + ", System.Collections.Immutable")
-                        ?? throw new InvalidOperationException($"Failed to resolve factory type for {fieldType.Name}");
-
-                    var createRangeMethod = factoryType
-                        .GetMethods(BindingFlags.Public | BindingFlags.Static)
-                        .FirstOrDefault(m =>
-                            m.Name == "CreateRange" &&
-                            m.IsGenericMethodDefinition &&
-                            m.GetParameters().Length == 1)
-                        ?? throw new InvalidOperationException($"Failed to get CreateRange method for {fieldType.Name}");
-
-                    var genericCreateRange = createRangeMethod.MakeGenericMethod(elementType);
-
-                    var typedList = typeof(Enumerable)
-                        .GetMethod("Cast")!
-                        .MakeGenericMethod(elementType)
-                        .Invoke(null, [values]);
-
-                    return genericCreateRange.Invoke(null, [typedList]) as IEnumerable
-                        ?? throw new InvalidOperationException($"Failed to create immutable collection for {fieldType.Name}");
+                    return CastToImmutable(fieldType, genericTypeDef, elementType, typedList);
                 }
+
                 else if (genericTypeDef == typeof(ReadOnlyCollection<>))
                 {
-                    var listType = typeof(List<>).MakeGenericType(elementType);
-                    var list = Activator.CreateInstance(listType);
-
-                    var typedList = typeof(Enumerable)
-                        .GetMethod("Cast")!
-                        .MakeGenericMethod(elementType)
-                        .Invoke(null, [values]);
-
-                    listType.GetMethod("AddRange")!.Invoke(list, [typedList]);
-                    var readOnlyCollection = Activator.CreateInstance(fieldType, list)!;
-                    return (IEnumerable)readOnlyCollection;
+                    return CastToReadOnlyCollection(fieldType, elementType, typedList);
                 }
             }
 
@@ -102,6 +50,74 @@ namespace FixtureBuilder.Helpers
                 addMethod.Invoke(collection, [item]);
 
             return collection;
+        }
+
+        private static Array CastToArray(Type fieldType, IEnumerable values)
+        {
+            var valuesList = values.Cast<object>().ToList();
+            var elementType = fieldType.GetElementType()!;
+            var array = Array.CreateInstance(elementType, valuesList.Count);
+            for (int i = 0; i < valuesList.Count; i++)
+            {
+                array.SetValue(valuesList[i], i);
+            }
+            return array;
+        }
+
+        private static Type GetConcreteType(Type interfaceType)
+        {
+            Type concreteType;
+            var genericTypeDef = interfaceType.GetGenericTypeDefinition();
+            var elementType = interfaceType.GetGenericArguments()[0];
+
+            if (genericTypeDef == typeof(IList<>) ||
+                genericTypeDef == typeof(IReadOnlyList<>) ||
+                genericTypeDef == typeof(IEnumerable<>) ||
+                genericTypeDef == typeof(IReadOnlyCollection<>) ||
+                genericTypeDef == typeof(ICollection<>))
+            {
+                concreteType = typeof(List<>);
+            }
+            else if (genericTypeDef == typeof(ISet<>) ||
+                     genericTypeDef == typeof(IReadOnlySet<>))
+            {
+                concreteType = typeof(HashSet<>);
+            }
+            else if (genericTypeDef == typeof(IImmutableList<>)) concreteType = typeof(ImmutableList<>);
+            else if (genericTypeDef == typeof(IImmutableSet<>)) concreteType = typeof(ImmutableHashSet<>);
+            else throw new InvalidOperationException($"Unsupported interface type: {interfaceType.Name}");
+
+            return concreteType.MakeGenericType(elementType);
+        }
+
+        private static IEnumerable CastToImmutable(Type fieldType, Type genericTypeDef, Type elementType, IEnumerable typedValues)
+        {
+            var factoryTypeName = genericTypeDef.FullName!.Replace("`1", "");
+            var factoryType = Type.GetType(factoryTypeName + ", System.Collections.Immutable")
+                ?? throw new InvalidOperationException($"Failed to resolve factory type for {fieldType.Name}");
+
+            var createRangeMethod = factoryType
+                .GetMethods(BindingFlags.Public | BindingFlags.Static)
+                .FirstOrDefault(m =>
+                    m.Name == "CreateRange" &&
+                    m.IsGenericMethodDefinition &&
+                    m.GetParameters().Length == 1)
+                ?? throw new InvalidOperationException($"Failed to get CreateRange method for {fieldType.Name}");
+
+            var genericCreateRange = createRangeMethod.MakeGenericMethod(elementType);
+
+            return genericCreateRange.Invoke(null, [typedValues]) as IEnumerable
+                ?? throw new InvalidOperationException($"Failed to create immutable collection for {fieldType.Name}");
+        }
+
+        private static IEnumerable CastToReadOnlyCollection(Type fieldType, Type elementType, IEnumerable typedValues)
+        {
+            var listType = typeof(List<>).MakeGenericType(elementType);
+            var list = Activator.CreateInstance(listType);
+
+            listType.GetMethod("AddRange")!.Invoke(list, [typedValues]);
+            var readOnlyCollection = Activator.CreateInstance(fieldType, list)!;
+            return (IEnumerable)readOnlyCollection;
         }
     }
 }
