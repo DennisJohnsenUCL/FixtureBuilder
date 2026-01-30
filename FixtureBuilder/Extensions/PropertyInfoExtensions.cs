5c6cd7affdd0d9c86e931ed714a23047c3c310f2
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2026-01-30 15:51:07 +0100

    Added an extension to PropertyInfo to match IsStatic on FieldInfo.
    It returns true if either get or set or static. This is okay since usually the whole property is static.

---
+++ FixtureBuilder/Extensions/PropertyInfoExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace FixtureBuilder.Extensions
+{
+    internal static class PropertyInfoExtensions
+    {
+        public static bool IsStatic(this PropertyInfo propertyInfo, bool nonPublic = false)
+        {
+            return propertyInfo.GetAccessors(nonPublic).Any(mi => mi.IsStatic);
+        }
+    }
+}
