6207db7deeecb983cbce69fb40952993cd78aac3
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2026-02-18 18:11:12 +0100

    Added ValidatingTypeLink to validate non-null target types for Type Links.

---
+++ FixtureBuilder/TypeLinks/ValidatingTypeLink.cs
@@ -0,0 +1,21 @@
+﻿namespace FixtureBuilder.TypeLinks
+{
+    internal class ValidatingTypeLink : ITypeLink
+    {
+        private readonly ITypeLink _inner;
+
+        public ValidatingTypeLink(ITypeLink inner)
+        {
+            ArgumentNullException.ThrowIfNull(inner);
+
+            _inner = inner;
+        }
+
+        public Type? Link(Type target)
+        {
+            ArgumentNullException.ThrowIfNull(target);
+
+            return _inner.Link(target);
+        }
+    }
+}
